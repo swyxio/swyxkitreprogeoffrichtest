7639e8f4676acf373f402b9dd325aa3473b39fe5
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-12-14 07:59:10 -0800

    Re-scaffold demo

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// extensions: ['.svelte', ...mdsvexConfig.extensions],
-
 	kit: {
 		adapter: adapter()
 	}
-
-	// preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
