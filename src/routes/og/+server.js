2d2a0b7f5333e78a0cc9c154c06c5322401e0bbf
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-12-15 12:00:36 -0800

    Use Svelte component

--- src/routes/og/+server.js
+++ src/routes/og/+server.js
@@ -1,20 +1,19 @@
 import satori from 'satori';
 import { Resvg } from '@resvg/resvg-js';
 import NotoSans from '$lib/NotoSans-Regular.ttf';
+import { html as toReactNode } from 'satori-html';
+import Card from '$lib/Card.svelte';
 
 const height = 630;
 const width = 1200;
 
 /** @type {import('./$types').RequestHandler} */
-export const GET = async () => {
-	const html = {
-		type: 'div',
-		props: {
-			children: 'hello, world',
-			style: { color: 'red' }
-		}
-	};
-	const svg = await satori(html, {
+export const GET = async ({ url }) => {
+	const message = url.searchParams.get('message') ?? undefined;
+	const result = Card.render({ message });
+	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
+
+	const svg = await satori(element, {
 		fonts: [
 			{
 				name: 'Noto Sans',
