a3ee88e5a091f1c65d2917bea196f7ebe52a39a2
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2022-12-15 12:09:15 -0800

    Dynamically update social card

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 	import 'prismjs/themes/prism-tomorrow.css';
 </script>
 
+<nav>
+	<a href="/">Home</a>
+	<a href="/md">mdsvex page</a>
+</nav>
+
 <slot />
+
+<style>
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
