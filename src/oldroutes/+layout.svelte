1349f9f0e042b197cc279a7e96312c45cd586c39
sw-yx <shawnthe1@gmail.com>
2023-01-03 05:02:38 -0500

    move over github imports

---
+++ src/oldroutes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../tailwind.css';
+	import 'prismjs/themes/prism-tomorrow.css';
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/md">mdsvex page</a>
+</nav>
+
+<slot />
+
+<style>
+	nav {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
